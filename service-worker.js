ffcfc64f66ea63cafec9c67770c8a7032d469140
Gabriel <rimoliga@gmail.com>
2025-08-31 00:52:27 +0000

    Make PWA installable and offline-capable

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "radioaficionados-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/buscador.js",
+  "/styles.css",
+  "/manifest.json",
+  "/data/listado_radioaficionados_unificado.json.gz",
+  "/favicon-192x192.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
